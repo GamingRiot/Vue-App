af552db703e70ae18ec05f4e5854f90f2187fe7e
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-05-25 18:51:24 +0530

    added files in repo

---
+++ main.js
@@ -0,0 +1,8 @@
+const app=Vue.createApp({
+    data(){
+        return {
+            description:'hello guys i am tushar arora and this is my first vue',
+            url:'https://web.whatsapp.com/'
+        }
+    }
+})
