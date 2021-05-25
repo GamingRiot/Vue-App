4e9d3e06b24918a52d445fe70ebcca61634a5a86
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-05-25 18:55:26 +0530

    image tested

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app=Vue.createApp({
     data(){
         return {
             description:'hello guys i am tushar arora and this is my first vue',
-            url:'https://web.whatsapp.com/'
+            url:'https://web.whatsapp.com/',
+            image:'./assets/dp.jpg'
         }
     }
 })
