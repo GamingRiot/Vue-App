8ca5457924ebc662ec4b156f2af448694a3c5adf
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-06-01 00:09:57 +0530

    components

--- main.js
+++ main.js
@@ -1,34 +1,7 @@
 const app=Vue.createApp({
     data(){
-        return {
-            description:'hello guys i am tushar arora and this is my first vue',
-            url:'https://web.whatsapp.com/',
-            brand:'vivo',
-            product:'m31',
-            image:'./assets/dp.jpg',
-            onSale:true,
-            inventory:'10',
-            sizes:['green','red'],
-            cart:0,
-            inStock:false,
-            styles:{
-                
-                fontSize:'14px'
-            }
+        return{
+            onSale:true
         }
-        
-    },
-    methods: {
-            addToCart(){
-                this.cart+=1
-           },
-            deleteFromCart(){
-                this.cart-=1
-            }
-        },
-        computed:{
-            title(){
-                return this.brand+' '+this.product
-            }
-        }
+    }
 })
