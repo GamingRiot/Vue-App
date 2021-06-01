fef645c204ed1ca4b603cb84ab7255d0575ffd42
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-06-02 03:09:38 +0530

    created forum discussion form

---
+++ components/Form.js
@@ -0,0 +1,12 @@
+app.component('form',{
+    props:,
+    template:
+    /*html*/
+    ` <div>
+    <h2>Would You recommend this product?</h2>
+
+    <label for="review">Yes/No</label>
+    <input type="text" id="review" />
+    <input type="submit" id="submit />
+    </div>`
+})
